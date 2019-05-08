7d305d838a662d177b233a3ff99bb342f119b09b
goreleaserbot <goreleaser@carlosbecker.com>
2019-05-08 16:06:06 -0700

    Brew formula update for vestibule version v1.2.7

--- Formula/vestibule.rb
+++ Formula/vestibule.rb
@@ -2,9 +2,9 @@
 class Vestibule < Formula
   desc "Gather secrets from various backends and inject them into the process environment"
   homepage "https://github.com/lumoslabs/vestibule"
-  url "https://github.com/lumoslabs/vestibule/releases/download/v1.2.6/vestibule_1.2.6_Darwin_x86_64.tar.gz"
-  version "1.2.6"
-  sha256 "0429f34c7553b42da6c55b10689e544da2441862f70501790f3005e7f6369c43"
+  url "https://github.com/lumoslabs/vestibule/releases/download/v1.2.7/vestibule_1.2.7_Darwin_x86_64.tar.gz"
+  version "1.2.7"
+  sha256 "5ee11ef53cbb7647e850915486965d674e621d524d88fefd372b3248e4baeeef"
 
   def install
     bin.install "vest"
